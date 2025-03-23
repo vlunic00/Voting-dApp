f0f0d5d41d9aada3b47606060b18f94f04b74927
vlunic00 <vlunic00@fesb.hr>
2025-03-23 17:34:07 +0100

    Connected app to wallet

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,46 @@
+"use client";
+
 import CandidateList from "@/components/CandidateList";
 import Header from "@/components/Header";
 import Link from 'next/link'
-import Web3 from "web3";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+
 
 
 export default function Home() {
-  const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+
+const [provider, setProvider] = useState<ethers.BrowserProvider | null>();
+const [signer, setSigner] = useState<ethers.Signer>();
+
+useEffect(() => {
+  async function initializeProvider() {
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        // Request account access
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+        const newProvider = new ethers.BrowserProvider(window.ethereum);
+        setProvider(newProvider);
+
+        const newSigner = await newProvider.getSigner();
+        setSigner(newSigner);
+      } catch (error) {
+        console.error('Error initializing provider/signer:', error);
+        // Handle the error (e.g., display an error message)
+      }
+    } else {
+      console.error('MetaMask or other Ethereum provider not found');
+      //Handle the lack of a provider.
+    }
+  }
+
+  initializeProvider();
+
+}, []);
+
+console.log(signer)
+
   return (
     <div className="flex flex-col pt-[10vh] items-center">
     <Header />
